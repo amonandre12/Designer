d0546d1ff5e637526f7c76950e878abdb7d6f7f8
amonandre12 <amonblinwadjaandre@gmail.com>
2025-05-15 08:00:07 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Selection des elements
+
+const burger = document.getElementById("burger")
+const navLinks = document.getElementById("nav-links")
+
+burger.addEventListener('click', (e) => {
+    if (navLinks.style.display === 'none') {
+        navLinks.style.display = 'block';
+        e.preventDefault()
+    } else {
+        navLinks.style.display = 'none'
+    }
+}) ;
+
+
+
+
+
